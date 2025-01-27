d905cc3ba587bf2d639280c3da4fb8b516267cef
SMartinez <46066859+smart79@users.noreply.github.com>
2025-01-27 09:04:46 -0600

    changed the root in index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create a root element
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
